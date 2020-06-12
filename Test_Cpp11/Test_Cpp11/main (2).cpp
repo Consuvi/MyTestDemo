0570926ad5d9cdb2da2b981079e9dbe900b3311c
consuvi <kongshaohui@live.com>
2020-06-12 21:53:09 +0800

    增加修改exe属性代码

---
+++ Test_Cpp11/Test_Cpp11/main (2).cpp
@@ -0,0 +1,305 @@
+
+#include "windows.h"
+#include "string"
+#include <iostream>
+using namespace std;
+#pragma comment(lib, "version.lib")
+
+std::string GetPEResource(const TCHAR* exepath, const TCHAR* type,
+	const TCHAR* name, int language = 0)
+{
+	std::string r = "";
+
+	if (!exepath)
+		return r;
+
+	//加载可执行文件
+	HMODULE hexe = LoadLibrary(exepath);
+	if (!hexe)
+		return r;
+
+	//查找资源
+	HRSRC src = FindResourceEx(hexe, type, name, language);
+	if (src)
+	{
+		HGLOBAL glb = LoadResource(hexe, src);
+		int sz = SizeofResource(hexe, src);
+		r = std::string((char *)LockResource(glb), sz);
+		UnlockResource(glb);
+		FreeResource(glb);
+	}
+
+	//释放可执行文件
+	FreeLibrary(hexe);
+
+	return r;
+}
+
+void PaddingBinary(wstring & in_data)
+{
+	int len = in_data.length();
+	if (len%2>0)
+	{
+		in_data.push_back((unsigned short)0);
+	}
+	if (in_data.back() != L'\0')
+	{
+		in_data.push_back(L'\0');
+		in_data.push_back(L'\0');
+	}
+}
+
+
+auto write_int = [](wstring & tmp, unsigned int v){
+	unsigned short wcs[3]{0};
+	//unsigned char cs[5]{0};
+// 	for (auto i = 0; i < 4; i++)
+// 	{
+// 		if ((cs[i] & 0x80) != 0)
+// 		{
+// 			cs[i] = 0x3f;
+// 		}
+// 	}
+	memcpy(wcs, &v, 4);
+	tmp.push_back(wcs[0]);
+	tmp.push_back(wcs[1]);
+};
+
+auto get_wchar = [](unsigned short us){
+	unsigned char cs[3]{0};
+	memcpy(cs, &us, 2);
+	wchar_t wc = 0;
+// 	for (auto i = 0; i < 2; i++)
+// 	{
+// 		if ((cs[i] & 0x80) != 0)
+// 		{
+// 			cs[i] = 0x3f;
+// 		}
+// 	}
+	memcpy(&wc, cs, 2);
+	return wc;
+};
+
+auto write_value = [](wstring & tmp, const wstring & key, const wstring & v){
+	wstring tmp1;
+	int len = v.length();
+	tmp1.append(wstring{ 0, (unsigned short)(len + 1), 1 });
+	tmp1.append(key);
+	PaddingBinary(tmp1);
+	tmp1.append(v);
+	tmp1.push_back(L'\0');
+	PaddingBinary(tmp1);
+	*tmp1.begin() = get_wchar(tmp1.size() * 2);
+	tmp.append(tmp1);
+};
+
+auto push_shorts = [](wstring & tmp, const wstring & str_in){
+	tmp.append(str_in);
+};
+
+
+void write_str(wstring & tmp, const wstring & v)
+{
+	tmp.append(v);
+	tmp.push_back(L'\0');
+}
+
+wstring GenVarFileInfo()
+{
+	wstring tmp;
+	//GenVarFileInfo
+	tmp.append(wstring{ 0, 0, 1 });
+	tmp += L"VarFileInfo";
+	PaddingBinary(tmp);
+	{
+		wstring tmp1;
+		//GenVar
+		push_shorts(tmp1, wstring{ 0, (unsigned short)4, 0 });
+		tmp1.append(L"Translation");
+		PaddingBinary(tmp1);
+		push_shorts(tmp1, wstring{ (unsigned short)0x4b0, (unsigned short)0x804 });
+		*tmp1.begin() = get_wchar(tmp1.size() * 2);
+		tmp.append(tmp1);
+	}
+	*tmp.begin() = get_wchar(tmp.size() * 2);
+	return tmp;
+}
+
+wstring GenFixedFileInfo()
+{
+	wstring tmp;
+	//GenFixedFileInfo
+	//tmp.append(wstring{ (unsigned short)0x04BD, (unsigned short)0xFEEF });
+	tmp.push_back(0x04BD);
+	tmp.push_back(0xFEEF);
+	write_int(tmp, 0x10000);
+	unsigned short v1 = 1, v2 = 6, v3 = 0, v4 = 88;
+	tmp.append(wstring{ v2, v1, v4, v3 });
+	tmp.append(wstring{ v2, v1, v4, v3 });
+	write_int(tmp, 0x3F);
+	write_int(tmp, 0);
+	write_int(tmp, 4);
+	write_int(tmp, 1);
+	write_int(tmp, 0);
+	write_int(tmp, 0);
+	write_int(tmp, 0);
+	return tmp;
+}
+
+wstring GenStringFileInfo()
+{
+	wstring tmp;
+	//GenStringFileInfo
+	tmp.append(wstring{ 0, 0, 1 });
+	tmp.append(L"StringFileInfo");
+	PaddingBinary(tmp);
+	{
+		wstring tmp1;
+		//GenStringTable
+		tmp1.append(wstring{ 0, 0, 1 });
+		tmp1.append(L"080404b0");
+		PaddingBinary(tmp1);
+		//write_value(tmp1, L"Comments", L"123");
+		write_value(tmp1, L"CompanyName", L"顺丰科技");
+		write_value(tmp1, L"FileDescription", L"FileDescription");
+		write_value(tmp1, L"FileVersion", L"1.6.0.88");
+		write_value(tmp1, L"InternalName", L"InternalName");
+		write_value(tmp1, L"LegalCopyright", L"LegalCopyright");
+		write_value(tmp1, L"OriginalFilename", L"ksh.exe");
+		write_value(tmp1, L"ProductName", L"ProductName");
+		write_value(tmp1, L"ProductVersion", L"1.6.0.88");
+		write_value(tmp1, L"Assembly Version", L"1.6.0.88");
+		*tmp1.begin() = get_wchar(tmp1.size() * 2);
+		tmp.append(tmp1);
+	}
+	*tmp.begin() = get_wchar(tmp.size() * 2);
+	return tmp;
+}
+
+void ConstructVerInfo(string & cdata)
+{
+	wstring data;
+	data.append(wstring{ 0, (unsigned short)0x34, 0 });
+	data.append(L"VS_VERSION_INFO");
+	PaddingBinary(data);
+
+	data += GenFixedFileInfo();
+	PaddingBinary(data);
+	data += GenVarFileInfo();
+	data += GenStringFileInfo();
+
+	DWORD sz = data.size() * 2;
+	*data.begin() = get_wchar(sz);
+	auto buf = new BYTE[sz+1]{0};
+	memcpy(buf, data.c_str(), sz);
+	cdata = string((char *)buf, sz);
+	delete[]buf;
+}
+
+
+void ModifyExeVerInfo(const wchar_t * lpszFile, LPVOID lpBuffer, DWORD dwSize)
+{
+	HANDLE hResource = BeginUpdateResource(lpszFile, FALSE);
+	if (NULL != hResource)
+	{
+		if (UpdateResource(hResource, RT_VERSION, MAKEINTRESOURCE(VS_VERSION_INFO), 0x804,
+			(LPVOID)lpBuffer, dwSize) != FALSE)
+		{
+			if (EndUpdateResource(hResource, FALSE) != FALSE)
+				return;
+		}
+	}
+}
+
+string read_data()
+{
+	string data;
+	auto f = fopen("e:\\rt_ver1", "rb");
+	if (f)
+	{
+		while (true)
+		{
+			char buf[1024]{0};
+			auto nRead = fread(buf, 1, 1023, f);
+			if (nRead == 0)
+			{
+				fclose(f);
+				break;
+			}
+			else
+			{
+				data += string(buf, nRead);
+			}
+		}
+	}
+	return data;
+}
+
+void write_to_file(const wstring & file_name, const string & data)
+{
+	if (!data.empty())
+	{
+		auto f = _wfopen(file_name.c_str(), L"wb+");
+		if (f)
+		{
+			fwrite(data.c_str(), 1, data.size(), f);
+		}
+		fclose(f);
+	}
+}
+
+
+void modify_lang(LPWSTR lpszFile)
+{
+	DWORD dwHandle = 0;
+	auto dwSize = GetFileVersionInfoSize(lpszFile, &dwHandle);
+	if (0 >= dwSize)
+	{
+		cout << GetLastError() << endl;
+		return;
+	}
+
+	LPBYTE lpBuffer = new BYTE[dwSize];
+	memset(lpBuffer, 0, dwSize);
+
+	if (GetFileVersionInfo(lpszFile, dwHandle, dwSize, lpBuffer) != FALSE)
+	{
+		if (VerQueryValue(lpBuffer, (L"\\VarFileInfo\\Translation"), (LPVOID *)&lpTranslate, &uTemp) != FALSE)
+		{
+
+		}
+		HANDLE hResource = BeginUpdateResource(lpszFile, FALSE);
+		if (NULL != hResource)
+		{
+			UINT uTemp;
+			DWORD dwVer[4] = { 0 };
+
+		}
+	}
+}
+
+int wmain(int argc, wchar_t * argv[])
+{
+	auto lpFilePath = L"D:\\Git\\esg-sfim-windows-std\\bin\\fslinkerpoc_.exe";
+	string data;	//data = read_data();
+	auto data1 = GetPEResource(L"D:\\Git\\esg-sfim-windows-std\\bin\\fslinkerstd_.exe",
+		RT_VERSION, MAKEINTRESOURCE(VS_VERSION_INFO), 0);
+	ConstructVerInfo(data);
+	cout << hex<<data.size() << endl;
+// 	unsigned int sz = data.size();
+// 	*(++data.begin()) = 0;
+// 	*data.begin() = sz;
+// 	BYTE buf[500]{0};
+// 	memcpy(buf, data.c_str(), data.size());
+// 	memcpy(buf, &sz, 4);
+// 	return 0;
+	//auto lpszFile = L"D:\\Git\\esg-sfim-windows-std\\bin\\fslinkerpoc_d.exe";
+	//data = GetPEResource(lpszFile, RT_VERSION, MAKEINTRESOURCE(VS_VERSION_INFO));
+
+	write_to_file(L"e:\\rt_ver1", data);
+	write_to_file(L"e:\\rt_ver2", data1);
+
+	ModifyExeVerInfo(lpFilePath, (LPVOID)data.c_str(), data.size());
+	system("pause");
+	return 0;
+}
---
+++ Test_Cpp11/Test_Cpp11/modify_exe_info.cpp
@@ -0,0 +1,254 @@
+
+#include "base.h"
+#pragma comment(lib, "version.lib")
+
+void modify_ver()
+{
+	auto lpRes = L"1.6.0.0";
+	wstring strApp = L"D:\\Git\\esg-sfim-windows-std\\bin\\fslinkerpoc_.exe";
+	HANDLE    hApp;
+	if (NULL == (hApp = ::BeginUpdateResource(strApp.c_str(), NULL)))
+	{
+		return;
+	}
+	//标记更新资源
+	::UpdateResource(hApp, RT_VERSION, L"ss", 0, (LPVOID)lpRes, wcslen(lpRes));
+	//写入新资源
+	int error = 0;
+	if (!::EndUpdateResource(hApp, FALSE))
+	{
+		error = GetLastError();
+		return;
+	}
+}
+void ReplaceExeIco(LPCTSTR lpszApp, LPCTSTR lpszIco)
+{
+	
+}
+
+struct
+{
+	WORD wLanguage;
+	WORD wCodePage;
+} *lpTranslate;
+
+
+void PaddingBinary(wstring & in_data)
+{
+	int len = in_data.length();
+	int a = len % 4;
+	if (a>0)
+	{
+		for (int i = 0; i < 4 - a; i++)
+		{
+			in_data.push_back((unsigned short)0);
+		}
+	}
+}
+
+wstring GenString(wstring key, wstring value)
+{
+	wstring tmp;
+	tmp.push_back(0);
+	tmp.push_back(value.length()+1);
+	tmp.push_back(1);
+	tmp += key;
+	PaddingBinary(tmp);
+	tmp += value;
+	PaddingBinary(tmp);
+	return tmp;
+}
+
+extern "C" __declspec(dllexport) void ModifyExeVerInfo(const char * lpszFile, LPVOID lpBuffer, DWORD dwSize)
+{
+	MessageBoxA(0, lpszFile, to_string((int)dwSize).c_str(), MB_OK);
+	HANDLE hResource = BeginUpdateResourceA(lpszFile, FALSE);
+	if (NULL != hResource)
+	{
+		if (UpdateResource(hResource, RT_VERSION, MAKEINTRESOURCE(VS_VERSION_INFO), 0,
+			(LPVOID)lpBuffer, dwSize) != FALSE)
+		{
+			if (EndUpdateResource(hResource, FALSE) != FALSE)
+				return;
+		}
+	}
+}
+
+BOOL UpdateDllFile()
+{
+	auto lpszFile = L"D:\\Git\\esg-sfim-windows-std\\bin\\fslinkerstd_.exe";
+
+	BOOL bRet = FALSE;
+	DWORD dwHandle = 0;
+	DWORD dwSize = 0;
+
+	dwSize = GetFileVersionInfoSize(lpszFile, &dwHandle);
+	if (0 >= dwSize)
+	{
+		cout << GetLastError() << endl;
+		return bRet;
+	}
+
+	LPBYTE lpBuffer = new BYTE[dwSize];
+	memset(lpBuffer, 0, dwSize);
+
+	if (GetFileVersionInfo(lpszFile, dwHandle, dwSize, lpBuffer) != FALSE)
+	{
+		HANDLE hResource = BeginUpdateResource(lpszFile, FALSE);
+		if (NULL != hResource)
+		{
+			UINT uTemp;
+			DWORD dwVer[4] = { 0 };
+
+			if (VerQueryValue(lpBuffer, (L"\\VarFileInfo\\Translation"), (LPVOID *)&lpTranslate, &uTemp) != FALSE)
+			{
+				// 修改版本信息,给副版本号加1
+				LPVOID lpFixedBuf = NULL;
+				DWORD dwFixedLen = 0;
+				if (FALSE != VerQueryValue(lpBuffer, (L"\\"), &lpFixedBuf, (PUINT)&dwFixedLen))
+				{
+					VS_FIXEDFILEINFO* pFixedInfo = (VS_FIXEDFILEINFO*)lpFixedBuf;
+
+					pFixedInfo->dwFileVersionLS = pFixedInfo->dwFileVersionLS + 0x1;
+					pFixedInfo->dwProductVersionLS = pFixedInfo->dwProductVersionLS + 0x1;
+
+					dwVer[0] = HIWORD(pFixedInfo->dwFileVersionMS);
+					dwVer[1] = LOWORD(pFixedInfo->dwFileVersionMS);
+					dwVer[2] = HIWORD(pFixedInfo->dwFileVersionLS);
+					dwVer[3] = LOWORD(pFixedInfo->dwFileVersionLS);
+				}
+
+				// 修改版本的文本信息
+				LPVOID lpStringBuf = NULL;
+				DWORD dwStringLen = 0;
+				TCHAR szTemp[MAX_PATH] = { 0 };
+				TCHAR szVersion[MAX_PATH] = { 0 };
+
+				TCHAR * szCompanyName = L"顺丰科技a";;
+				TCHAR * szFileDescription = L"丰 声s";
+				TCHAR * szFileVersion = L"5.9.0.0";
+				TCHAR * szInternalName = L"fslinkerstd_.exe";
+				TCHAR * szLegalCopyright = L"顺丰科技公司 Copyright (C) 2017";
+				TCHAR * szOriginalFilename = L"ksh.exe";
+				TCHAR * szProductName = L"丰 声";
+				TCHAR * szProductVersion = L"5.6.0.0";
+				
+				/*
+				 BinaryWriter bw = CreateBinaryStream();
+				 bw.Write((ushort)0);
+				 bw.Write((ushort)0);
+				 bw.Write((ushort)1);
+				 AppendUnicodeString(bw, "000004b0");
+				 PaddingBinary(bw);
+				 bw.Write(GenString("Comments", Comments));
+				 bw.Write(GenString("CompanyName", CompanyName));
+				 bw.Write(GenString("FileDescription", FileDescription));
+				 bw.Write(GenString("FileVersion", VersionString));
+				 bw.Write(GenString("InternalName", InternalName));
+				 bw.Write(GenString("LegalCopyright", LegalCopyright));
+				 bw.Write(GenString("OriginalFilename", OriginalFilename));
+				 bw.Write(GenString("ProductName", ProductName));
+				 bw.Write(GenString("ProductVersion", VersionString));
+				 bw.Write(GenString("Assembly Version", VersionString));*/
+
+				wstring tmp;
+				tmp.clear();
+				tmp.push_back((unsigned short)0);
+				tmp.push_back((unsigned short)0);
+				tmp.push_back((unsigned short)1);
+				tmp += L"000004b0";
+				PaddingBinary(tmp);
+				tmp.append(GenString(L"CompanyName", L"sss"));
+
+// 				wsprintf(szTemp, L"\\StringFileInfo\\%04x%04x\\", lpTranslate[0].wLanguage, lpTranslate[0].wCodePage);
+// 				wstring szBlockLang = szTemp;
+// 
+// 				if (FALSE != VerQueryValue(lpBuffer, (szBlockLang + L"CompanyName").c_str(), &lpStringBuf, (PUINT)&dwStringLen))
+// 					memcpy(lpStringBuf, szCompanyName, (wcslen(szCompanyName) + 1) * sizeof(TCHAR));
+// 
+// 				if (FALSE != VerQueryValue(lpBuffer, (szBlockLang + L"FileDescription").c_str(), &lpStringBuf, (PUINT)&dwStringLen))
+// 					memcpy(lpStringBuf, szFileDescription, (wcslen(szFileDescription) + 1) * sizeof(TCHAR));
+// 
+// 				if (FALSE != VerQueryValue(lpBuffer, (szBlockLang + L"FileVersion").c_str(), &lpStringBuf, (PUINT)&dwStringLen))
+// 					memcpy(lpStringBuf, szFileVersion, (wcslen(szFileVersion) + 1) * sizeof(TCHAR));
+// 
+// 				if (FALSE != VerQueryValue(lpBuffer, (szBlockLang+L"InternalName").c_str(), &lpStringBuf, (PUINT)&dwStringLen))
+// 					memcpy(lpStringBuf, szInternalName, (wcslen(szInternalName) + 1) * sizeof(TCHAR));
+// 
+// 				if (FALSE != VerQueryValue(lpBuffer, (szBlockLang + L"LegalCopyright").c_str(), &lpStringBuf, (PUINT)&dwStringLen))
+// 					memcpy(lpStringBuf, szLegalCopyright, (wcslen(szLegalCopyright) + 1) * sizeof(TCHAR));
+// 
+// 				if (FALSE != VerQueryValue(lpBuffer, (szBlockLang + L"OriginalFilename").c_str(), &lpStringBuf, (PUINT)&dwStringLen))
+// 					memcpy(lpStringBuf, szOriginalFilename, (wcslen(szOriginalFilename) + 1) * sizeof(TCHAR));
+// 
+// 				if (FALSE != VerQueryValue(lpBuffer, (szBlockLang + L"ProductName").c_str(), &lpStringBuf, (PUINT)&dwStringLen))
+// 					memcpy(lpStringBuf, szProductName, (wcslen(szProductName) + 1) * sizeof(TCHAR));
+// 
+// // 				if (FALSE != VerQueryValue(lpBuffer, (szBlockLang + L"ProductVersion").c_str(), &lpStringBuf, (PUINT)&dwStringLen))
+// // 					memcpy(lpStringBuf, szProductVersion, (wcslen(szProductVersion) + 1) * sizeof(TCHAR));
+// 				
+// 				memset(szTemp, 0, sizeof(szTemp));
+// 				wsprintf(szVersion, (L"%d, %d, %d, %d"), dwVer[0], dwVer[1], dwVer[2], dwVer[3]);
+// 
+// 				memset(szTemp, 0, sizeof(szTemp));
+// 				wsprintf(szTemp, (L"\\StringFileInfo\\%04x%04x\\FileVersion"), lpTranslate->wLanguage, lpTranslate->wCodePage);
+// 				
+// 				if (FALSE != VerQueryValue(lpBuffer, szTemp, &lpStringBuf, (PUINT)&dwStringLen))
+// 					memcpy(lpStringBuf, szVersion, (wcslen(szVersion) + 1) * sizeof(TCHAR));
+// 				else
+// 				{
+// 					cout << GetLastError()<<endl;
+// 				}
+				// 更新
+				if (UpdateResource(hResource, RT_VERSION, MAKEINTRESOURCE(VS_VERSION_INFO), lpTranslate->wLanguage, 
+					lpBuffer, dwSize) != FALSE)
+				{
+					if (EndUpdateResource(hResource, FALSE) != FALSE)
+						bRet = TRUE;
+				}
+			}
+		}
+	}
+
+	if (lpBuffer)
+		delete[] lpBuffer;
+
+	return bRet;
+}
+
+std::string GetPEResource(const TCHAR* exepath, const TCHAR* type,
+	const TCHAR* name, int language = 0)
+{
+	std::string r = "";
+
+	if (!exepath)
+		return r;
+
+	//加载可执行文件
+	HMODULE hexe = LoadLibrary(exepath);
+	if (!hexe)
+		return r;
+
+	//查找资源
+	HRSRC src = FindResourceEx(hexe, type, name, language);
+	if (src)
+	{
+		HGLOBAL glb = LoadResource(hexe, src);
+		int sz = SizeofResource(hexe, src);
+		r = std::string((char *)LockResource(glb), sz);
+		UnlockResource(glb);
+		FreeResource(glb);
+	}
+
+	//释放可执行文件
+	FreeLibrary(hexe);
+
+	return r;
+}
+
+
+void test20200610()
+{
+	auto lpszFile = L"D:\\Git\\esg-sfim-windows-std\\bin\\fslinkerstd_.exe";
+	GetPEResource(lpszFile, RT_VERSION, MAKEINTRESOURCE(VS_VERSION_INFO));
+}
